3325088baaff1ee3a8be871c0c2e8bc1bcac47cf
yichunli95 <yichunli95@gmail.com>
2021-04-18 18:50:52 -0400

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     # './data/train.csv'
     params = {'base_path':            '.',     # NOTE: base path of the environment.
               'min_points_threshold': 1,     # NOTE: Minimum number of instances of an atom to be considered as a label. Atoms with less than this value are considered "other".
-              'n_sample_hard':        1000000, 
+              'n_sample_hard':        2000000, 
               'n_sample_per_label':   50000,   # NOTE: applies to both train and validation sets.
               'overwrite':            False,   # NOTE: determines if we overwrite existing data.
               'input_format':         "RGB",   # NOTE: Important to set depending on data format!
@@ -31,16 +31,18 @@ if __name__ == '__main__':
 
     # construct trainer object
     model = trainer.Trainer(params=params)
+    
+    print("Pre-processing finished.")
 
-    # train model
-    if train_new_model:
-        model.train(train_params=train_params)
+#     # train model
+#     if train_new_model:
+#         model.train(train_params=train_params)
 
-    # restore trained model
-    model.load_model(checkpoint_name, device=device)
+#     # restore trained model
+#     model.load_model(checkpoint_name, device=device)
 
-    # predict test images per batch in parallel
-    results = model.predict_batch(images_path=test_data_path)
+#     # predict test images per batch in parallel
+#     results = model.predict_batch(images_path=test_data_path)
 
-    # saves predictions to
-    results.to_csv(f'{predictions_path}/predictions_{model_name}.csv', index=False)
+#     # saves predictions to
+#     results.to_csv(f'{predictions_path}/predictions_{model_name}.csv', index=False)
