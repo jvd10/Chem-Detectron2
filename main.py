1b7b61c9ad238c08778e52a1c117d3848b7994b0
yichunli95 <yichunli95@gmail.com>
2021-05-19 16:06:36 -0400

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
     checkpoint_name = 'model_final.pth'
     predictions_path = './predictions'
     device = 'cuda'
-    base_path = '/ocean/projects/tra190016p/ylix/dacon_old/dacon'
+    base_path = '/ocean/projects/tra190016p/ylix/dacon'
     # general parameters
     # './data/train.csv'
     params = {'base_path':            base_path,     # NOTE: base path of the environment.
@@ -57,46 +57,3 @@ if __name__ == '__main__':
     # # predict test images per batch in parallel
     results = tr.predict_batch(images_path=test_data_path, batch_size=batch_size)
 
-    # # saves predictions to
-    #results.to_csv(f'{predictions_path}/predictions_{model_name}.csv', index=False)
-    print("We finished.")
-
-    # # DACON2OSCAR
-    # #images = ImageList.from_tensors(...)  # preprocessed input tensor
-    # predictor = tr.predictor
-    #     # with torch.no_grad():
-    #     #     # Apply pre-processing to image.
-    #     #     if self.input_format == "RGB":
-    #     #         # whether the model expects BGR inputs or RGB
-    #     #         images_batch = [img[:, :, ::-1] for img in images_batch]
-    #     #     height, width = images_batch[0].shape[:2]
-    #     #     inputs = []
-    #     #     for image in images_batch:
-    #     #         image = self.aug.get_transform(image).apply_image(image)
-    #     #         image = torch.as_tensor(image.astype("float32").transpose(2, 0, 1))
-    #     #         inputs.append({"image": image, "height": height, "width": width})
-    #     #     predictions = self.model(inputs)
-    #     #     return predictions
-    # images = None
-    # images_paths = glob.glob(base_path + f'{test_data_path}/*.png')
-    # for i in tqdm(range(0, len(images_paths), batch_size)):
-    #             # input format for the model list[{"image"}: ...,], image: Tensor, image in (C, H, W) format.
-    #     images = [cv2.resize(cv2.imread(path), (300,300))[:, :, ::-1] for path in images_paths[i:i + batch_size]]
-    # if predictor.input_format == "RGB":
-    #     images = [img[:, :, ::-1] for img in images]
-    # for i, _ in enumerate(images):
-    #     images[i] = images[i].astype("float32").transpose(2, 0, 1)
-    # # for a in images:
-    # #     print(a.shape)
-    # images = torch.tensor(images)
-    # print(images.shape)
-    # predictor.model.eval()
-    # with torch.no_grad():
-    #     images = images.to(device)
-    #     features = predictor.model.backbone(images)
-    #     proposals, _ = predictor.model.proposal_generator(images, features)
-    #     instances, _ = predictor.model.roi_heads(images, features, proposals)
-    #     mask_features = [features[f] for f in predictor.model.roi_heads.in_features]
-    #     mask_features = predictor.model.roi_heads.mask_pooler(mask_features, [x.pred_boxes for x in instances])
-    #     print(mask_features.shape)
-    #sys.exit()
